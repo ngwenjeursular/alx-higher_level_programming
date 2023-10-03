730cb85cba37629db5df7a4a3bfba180c3ac7e17
ngwenjeursular <ngwenjeursular@gmail.com>
2023-10-03 13:17:55 -0700

    fixed a pycode style error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
 
-"""Prints all possible different combinations of two digits in ascending order."""
+"""Prints all possible diff combinations of two digits in ascending order."""
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 == 8 and digit2 == 9:
