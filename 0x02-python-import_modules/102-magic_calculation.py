23c89ffa14ceac85dcaef17fc9c8508e824ffa83
ngwenjeursular <ngwenjeursular@gmail.com>
2023-10-05 13:53:48 -0700

    fix err

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-
-
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
+def magic_calculation(a, b, c):
+    if b > a:
         return (c)
 
-    else:
-        return(sub(a, b))
+    if b < c:
+        return (a + b)
+
+    return ((a * b) - c)
