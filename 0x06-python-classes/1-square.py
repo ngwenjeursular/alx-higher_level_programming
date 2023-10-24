031463fa74e2392204e6858f69ec96ed548b43b0
ngwenjeursular <ngwenjeursular@gmail.com>
2023-10-24 12:49:16 -0700

    a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Constructor to initialize a square with the given side length
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size
