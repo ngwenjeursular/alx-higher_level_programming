616fbd3515cbc8c4295cb1df88cf91420e82d7b5
ngwenjeursular <ngwenjeursular@gmail.com>
2023-10-24 12:31:45 -0700

    empty class Square that defines a square:

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A module for square operations."""
+
+
+class Square:
+    """A class representing a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Note:
+        This class is a basic representation of a square.
+    """
+    pass
