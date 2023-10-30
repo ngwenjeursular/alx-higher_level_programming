b1d226417b4160937f328efa567f399ede018014
ngwenjeursular <ngwenjeursular@gmail.com>
2023-10-30 08:34:23 -0700

    empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module creates an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    empty class for now
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# 0x08. Python - More Classes and Objects
+
+Learning about OOP (Object Oriented programming)
