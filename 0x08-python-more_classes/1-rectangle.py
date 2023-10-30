85e05dd3fd53d38e98389542fcac254af7a6ae22
ngwenjeursular <ngwenjeursular@gmail.com>
2023-10-30 13:43:27 -0700

    1. Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+This module writes a class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    defines a rectangle using: Private instance attribute: width, height
+    Which are the two arguments
+
+    Args:
+        width(int) : measure of one of the two parallel sides
+        height(int) : measure of the other vertial side
+
+    Raises:
+        TypeError:
+                    if width is not an integer
+                    if height is not an integer
+        ValueError:
+                    if height is less than 0
+                    if width is less than 0
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """__width getter.
+
+        Returns:
+            __width (int): horizontal dimension of rectangle
+
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Args:
+            value (int): horizontal dimension of rectangle
+
+        Attributes:
+            __width (int): horizontal dimension of rectangle
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+
+        """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """__height getter.
+
+        Returns:
+            __height (int): vertical dimension of rectangle
+
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Args:
+            value (int): vertical dimension of rectangle
+
+        Attributes:
+            __height (int): vertical dimension of rectangle
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+
+        """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
