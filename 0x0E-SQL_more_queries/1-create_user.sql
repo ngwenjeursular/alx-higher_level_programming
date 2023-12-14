cfe61b9c32bf87db73e688d251b7f859039102f6
ngwenjeursular <ngwenjeursular@gmail.com>
2023-12-14 12:13:27 -0800

    correction of task 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,10 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- creates the database hbtn_0d_2 and the user user_0d_2
+   -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+   -- The user_0d_2 password should be set to user_0d_2_pwd
+   -- If the database hbtn_0d_2 already exists, your script should not fail
+   -- If the user user_0d_2 already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
